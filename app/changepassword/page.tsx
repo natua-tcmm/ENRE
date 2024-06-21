cee6103c97f29ce196a6698bc80c16aafb1fd391
OkamotoShu <okamotoshu11@gmail.com>
2024-06-21 13:11:12 +0900

    パスワード変更画面のフッター削除

--- app/changepassword/page.tsx
+++ app/changepassword/page.tsx
@@ -11,7 +11,7 @@ export default function ChangePassword() {
         <HeaderComponent />
       </Suspense>
       <ChangePasswordComponent />;
-      <FooterComponent />
+      {/* <FooterComponent /> */}
     </main>
   );
 }
