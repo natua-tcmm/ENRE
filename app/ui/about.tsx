b638242bbc363b2c7628f572b05ea7241bdaa760
OkamotoShu <okamotoshu11@gmail.com>
2024-06-23 19:05:30 +0900

    aboutの内容変更

--- app/ui/about.tsx
+++ app/ui/about.tsx
@@ -1,56 +1,84 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function AboutComponent() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen px-3">
-      <h2 className="text-2xl font-bold">Enreとは?</h2>
-      <p className="text-lg text-center font-bold text-green-700 mb-0">
-        京都産業大学は、とても広いです。
+      <Image
+        src="/applicationIcon_512.png"
+        width={250}
+        height={250}
+        alt="charactor"
+        priority
+      />
+      <Image src="/title.png" width={250} height={150} alt="title" />
+      <h2 className="text-xl font-bold mt-5">しぜんとひとをつなぐ”Enre”って？</h2>
+      <h2 className="text-xl font-bold mt-5">あなた史上最高のキャンパスライフを。</h2>
+      <p className="text-lg text-center">
+        混雑時の待機時間や空き時間を有効活用して、 気軽にイベント検索・参加・記録が可能なWebアプリ”Enre”を開発しました。学内には、様々なイベントや豊かな自然環境、学生による活動が満載です。
       </p>
-      <p className="text-lg">
-        きっと学内には、未だ行ったことのない施設や、自然環境、コミュニティが沢山あります…
+      <p className="text-lg text-center mt-5">
+        せっかくのイベント、せっかくのONE CAMPUS。きっと行ったことのない施設やイベント、自然環境、新たなコミュニティがたくさんあるはず！
       </p>
-      <p className="text-lg">
-        普段の空いた時間や混雑時間を活用して、大学にある多様な出会いと様々な活動経験により、
-        もっと
-        <span className="font-bold no-underline text-green-700">
-          有意義な時間
-        </span>
-        を創りたい！と考えました。そんな目標を目指して、
-        学内で行われている様々なイベント、社会・環境活動を共有するWebアプリ「
-        <span className="font-bold no-underline text-green-700">
-          Enre（ver1.1.0）
-        </span>
-        」をつくりました…
+      <p className="text-lg text-center mt-5">
+        無限大の可能性を秘めたキャンパスライフをさらに有意義にしてみませんか？
       </p>
-      <p className="text-lg">
-        「
-        <span className="font-bold no-underline text-green-700">
-          Enre（エンリー）
-        </span>
-        」を使って、毎日に新たな出会いと経験を！
+      <h2 className="text-2xl font-bold mt-5">Enre（ver. 2.0.0）の概要</h2>
+      <p className="text-lg text-center mt-5">
+        Enre（ver2.0.0）は2024年7月1日～7月5日のEnre weekを皮切りにEnre（ver1.1.0）は7月31日まで使用することが可能になっています。
       </p>
-      <h2 className="text-2xl font-bold mt-5">Enre（ver. 1.1.0）の概要</h2>
-      <p className="text-lg">
-        Enre（ver1.1.0）は、2024年7月1日〜5日に開催される
-        <span className="font-bold no-underline text-green-700">
-          学内のイベント・社会環境活動 （プレイベント）を案内します
-        </span>
-        （Enreの利用は31日まで）。Enreに登録すると、 自身のイベント・活動の
-        <span className="font-bold no-underline text-green-700">
-          経験や貢献が、Enre上でポイントとして把握できます。
-        </span>
-        ポイントが貯まると、カエルのアイコン（がまちゃん）が成長します。
+      <p className="text-lg text-center mt-5">
+        Enreを使うことであなたの様々なイベントや活動への参加記録がEnreポイントやポイントを貯めることで成長するEnre公式キャラクター”ガマちゃん”によって簡単に把握することが出来ます。
       </p>
-      <p className="text-lg">
-        Enre（ver1.1.0）は、 試行版ですが、今回のプレイベントを踏まえて、
-        Enreは、大学や他の地域において、イベント・環境活動への参加を促すツールとして開発を進めたいです。
+      <p className="text-lg text-center mt-5">
+        更に、今回のEnre（ver2.0.0）ではver1.1.0には無かった機能が追加予定です！
       </p>
-      <p className="text-lg">
-        皆さんの意見や参加をもとに、改良を重ねたいと思いますので、ぜひご協力ください。
+      <p className="text-lg text-center mt-5">
+        ・混雑状況が確認できる
+      </p>
+      <p className="text-lg font-bold text-center">
+        「Enreマップ」
+      </p>
+      <p className="text-lg text-center mt-5">
+        ・イベント情報が一覧で見れる
+      </p>
+      <p className="text-lg font-bold text-center">
+        「イベントリスト」
+      </p>
+      <p className="text-lg text-center mt-5">
+        Enreはこれからも皆様と共に成長し、大学や様々な地域をイベントや環境貢献行動などを促すことで活性化するツールとして進んでいきます。
+      </p>
+      <p className="text-lg text-center mt-5">
+        皆様の意見や参加をもとに今後もアップデートを続けていきます。ぜひご協力をお願いいたします。
+      </p>
+      <p className="text-lg font-bold text-center mt-5">
+        さあ、はじめよう”Enre”！
+      </p>
+      <p className="text-lg font-bold text-center mt-5"></p>
+      <Image src="/title2.png" width={300} height={150} alt="title" />
+      <p className="text-lg font-bold text-center mt-3">
+        Enre week開催!!
+      </p>
+      <p className="text-lg font-bold text-center">
+        7/1～7/5
+      </p>
+      <p className="text-lg text-center mt-5">
+        Enreの実証実験を兼ねたリニューアルしたEnreのリリースを7月1日（月）～5日（金）の5日間で開催します。
+      </p>
+      <p className="text-lg text-center mt-5">
+        ・様々な種類のイベントが一挙に集約
+      </p>
+      <p className="text-lg text-center">
+         Enreを使ってイベントを探して参加・記録しよう！
+      </p>
+      <p className="text-lg text-center mt-5">
+         ・イベント参加でEnreポイントがゲット可能
+      </p>
+      <p className="text-lg text-center">
+         ポイントゲットでガマちゃんを成長させよう！
       </p>
       <h2 className="text-2xl font-bold mt-5">企画・制作</h2>
-      <p className="text-lg">
+      <p className="text-lg text-center">
         本取組は、グリーンインフラの社会実装に向けた研究活動として、以下の２つの研究室が協働しておこなっています。
       </p>
       <ul className="text-lg">
@@ -61,11 +89,13 @@ export default function AboutComponent() {
           京都産業大学 情報理工学部 情報理工学科 棟方研究室
         </li>
       </ul>
-      <p className="text-sm">
+      <p className="text-sm text-center">
         なお、本取組は、内閣府SIPスマートインフラマネジメントシステムの構築e-1「魅力的な国土・都市・地域づくりを評価するグリーンインフラに関する省庁連携基盤」の研究活動の一環です。
       </p>
+      <Image src="/sip.png" width={100} height={150} alt="title" />
       <h2 className="text-2xl font-bold mt-5">お問い合わせ先</h2>
       <p className="text-lg">nisidalab@gmail.com</p>
+      <p className="text-lg">（担当者：三鬼）</p>
       <Link href={"/"} className="no-underline text-white mt-4">
         <button className="text-lg bg-green-700 p-2 rounded-md">
           ホームに戻る
