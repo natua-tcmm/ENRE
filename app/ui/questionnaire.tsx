5f1cadb3d9bab807d5dd54b2f9c1397524ac8390
natua-tcmm <natua.tcmm@gmail.com>
2024-06-27 18:37:42 +0900

    アンケート背景色変えた

--- app/ui/questionnaire.tsx
+++ app/ui/questionnaire.tsx
@@ -17,7 +17,7 @@ export default function QuestionnaireComponent({ link, title }: Props) {
     );
   };
   return (
-    <div className="w-full bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div className="w-full bg-yellow-100 rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="grid grid-rows-max-content-layout-2 grid-cols-max-content-layout-2 gap-2 p-3">
         <div className="row-start-1 col-start-1 col-end-3 justify-items-center items-center text-center uppercase tracking-wide text-sm text-green-700 font-semibold">
           {title}
