326bbb0e37ce905819ebb3f6d13b4338223ac6e4
OkamotoShu <okamotoshu11@gmail.com>
2024-06-28 04:05:24 +0900

    使わないページをコメントアウト

--- app/ui/googleMap.tsx
+++ app/ui/googleMap.tsx
@@ -1,106 +1,106 @@
-"use client";
+// "use client";
 
-import { GoogleMap } from "@react-google-maps/api";
-import { useJsApiLoader } from "@react-google-maps/api";
-import { useCallback, useState, useRef } from "react";
-import DetailCardComponent from "./detailCard";
-import { LoadingAnimation } from "./skeletons";
+// import { GoogleMap } from "@react-google-maps/api";
+// import { useJsApiLoader } from "@react-google-maps/api";
+// import { useCallback, useState, useRef } from "react";
+// import DetailCardComponent from "./detailCard";
+// import { LoadingAnimation } from "./skeletons";
 
-type Map = google.maps.Map;
+// type Map = google.maps.Map;
 
-const googleMapOptions = {
-  mapTypeControl: false,
-  streetViewControl: false,
-  fullscreenControl: false,
-  zoomControl: false,
-  language: "ja",
-};
+// const googleMapOptions = {
+//   mapTypeControl: false,
+//   streetViewControl: false,
+//   fullscreenControl: false,
+//   zoomControl: false,
+//   language: "ja",
+// };
 
-type Spots = {
-  spotsCenter: { lat: number; lng: number }[];
-  spotsInfo: {
-    title: string;
-    content: string;
-    place: string;
-    link?: string;
-    process: string[];
-    caution: string[];
-    condition: string[];
-  }[];
-};
+// type Spots = {
+//   spotsCenter: { lat: number; lng: number }[];
+//   spotsInfo: {
+//     title: string;
+//     content: string;
+//     place: string;
+//     link?: string;
+//     process: string[];
+//     caution: string[];
+//     condition: string[];
+//   }[];
+// };
 
-export default function GoogleMapComponent({ spotsCenter, spotsInfo }: Spots) {
-  const [spotInfo, setSpotInfo] = useState(spotsInfo[0]);
-  const wrapperHeight = `calc(100vh - 160px)`;
-  const containerStyle = {
-    width: "100%",
-    height: wrapperHeight,
-  };
+// export default function GoogleMapComponent({ spotsCenter, spotsInfo }: Spots) {
+//   const [spotInfo, setSpotInfo] = useState(spotsInfo[0]);
+//   const wrapperHeight = `calc(100vh - 160px)`;
+//   const containerStyle = {
+//     width: "100%",
+//     height: wrapperHeight,
+//   };
 
-  const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map",
-    googleMapsApiKey,
-  });
+//   const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
+//   const { isLoaded } = useJsApiLoader({
+//     id: "google-map",
+//     googleMapsApiKey,
+//   });
 
-  const mapRef = useRef<Map>();
-  const onLoad = useCallback(
-    (map: Map) => {
-      mapRef.current = map;
-      const bounds = new window.google.maps.LatLngBounds();
-      spotsCenter.forEach((spot, index) => {
-        const marker = new window.google.maps.Marker({
-          position: {
-            lat: spot.lat,
-            lng: spot.lng,
-          },
-          map,
-        });
-        const infoWindow = new window.google.maps.InfoWindow({
-          content: `<p>${spotsInfo[index].title}</p>`,
-        });
-        marker.addListener("click", () => {
-          map.panTo(spot);
-          setSpotInfo(spotsInfo[index]);
-          infoWindow.open(map, marker);
-        });
-        bounds.extend(spot);
-      });
-      spotsCenter.length > 2 && map.fitBounds(bounds, 100);
-    },
-    [spotsCenter, spotsInfo]
-  );
+//   const mapRef = useRef<Map>();
+//   const onLoad = useCallback(
+//     (map: Map) => {
+//       mapRef.current = map;
+//       const bounds = new window.google.maps.LatLngBounds();
+//       spotsCenter.forEach((spot, index) => {
+//         const marker = new window.google.maps.Marker({
+//           position: {
+//             lat: spot.lat,
+//             lng: spot.lng,
+//           },
+//           map,
+//         });
+//         const infoWindow = new window.google.maps.InfoWindow({
+//           content: `<p>${spotsInfo[index].title}</p>`,
+//         });
+//         marker.addListener("click", () => {
+//           map.panTo(spot);
+//           setSpotInfo(spotsInfo[index]);
+//           infoWindow.open(map, marker);
+//         });
+//         bounds.extend(spot);
+//       });
+//       spotsCenter.length > 2 && map.fitBounds(bounds, 100);
+//     },
+//     [spotsCenter, spotsInfo]
+//   );
 
-  return (
-    <>
-      {isLoaded ? (
-        <>
-          <div className="absolute z-10 p-1 w-full md:w-5/12 items-center">
-            <DetailCardComponent
-              spotInfo={spotInfo}
-              thema="dark"
-              textColor="white"
-            />
-          </div>
-          {spotsCenter.length <= 2 ? (
-            <GoogleMap
-              zoom={17}
-              center={spotsCenter[0]}
-              options={googleMapOptions}
-              mapContainerStyle={containerStyle}
-              onLoad={onLoad}
-            ></GoogleMap>
-          ) : (
-            <GoogleMap
-              options={googleMapOptions}
-              mapContainerStyle={containerStyle}
-              onLoad={onLoad}
-            ></GoogleMap>
-          )}
-        </>
-      ) : (
-        <LoadingAnimation />
-      )}
-    </>
-  );
-}
+//   return (
+//     <>
+//       {isLoaded ? (
+//         <>
+//           <div className="absolute z-10 p-1 w-full md:w-5/12 items-center">
+//             <DetailCardComponent
+//               spotInfo={spotInfo}
+//               thema="dark"
+//               textColor="white"
+//             />
+//           </div>
+//           {spotsCenter.length <= 2 ? (
+//             <GoogleMap
+//               zoom={17}
+//               center={spotsCenter[0]}
+//               options={googleMapOptions}
+//               mapContainerStyle={containerStyle}
+//               onLoad={onLoad}
+//             ></GoogleMap>
+//           ) : (
+//             <GoogleMap
+//               options={googleMapOptions}
+//               mapContainerStyle={containerStyle}
+//               onLoad={onLoad}
+//             ></GoogleMap>
+//           )}
+//         </>
+//       ) : (
+//         <LoadingAnimation />
+//       )}
+//     </>
+//   );
+// }
