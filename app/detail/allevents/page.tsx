a8c1d2b010d9bb6443987f1e2341d7ce5a02b4d7
OkamotoShu <okamotoshu11@gmail.com>
2024-06-28 04:08:41 +0900

    使っていないページのコメントアウト

--- app/detail/allevents/page.tsx
+++ app/detail/allevents/page.tsx
@@ -1,20 +1,20 @@
 // 使ってないよ！！！！！！！！！！！！！！！！
-import HeaderComponent from "@/app/ui/header";
-import FooterComponent from "@/app/ui/footer";
-import EventDetailComponent from "@/app/ui/eventDetail";
-import { Suspense } from "react";
-import { HeaderSkeleton, EventDetailSkeleton } from "@/app/ui/skeletons";
+// import HeaderComponent from "@/app/ui/header";
+// import FooterComponent from "@/app/ui/footer";
+// import EventDetailComponent from "@/app/ui/eventDetail";
+// import { Suspense } from "react";
+// import { HeaderSkeleton, EventDetailSkeleton } from "@/app/ui/skeletons";
 
-export default function AllEventsDetail() {
-  return (
-    <main className="grid grid-rows-base-layout h-screen w-full">
-      <Suspense fallback={<HeaderSkeleton />}>
-        <HeaderComponent />
-      </Suspense>
-      <Suspense fallback={<EventDetailSkeleton />}>
-        <EventDetailComponent />
-      </Suspense>
-      <FooterComponent />
-    </main>
-  );
-}
+// export default function AllEventsDetail() {
+//   return (
+//     <main className="grid grid-rows-base-layout h-screen w-full">
+//       <Suspense fallback={<HeaderSkeleton />}>
+//         <HeaderComponent />
+//       </Suspense>
+//       <Suspense fallback={<EventDetailSkeleton />}>
+//         <EventDetailComponent />
+//       </Suspense>
+//       <FooterComponent />
+//     </main>
+//   );
+// }
